21b1a1bb9a896ac669855b3decdbda159ab10140
sagafonoff <sandor.agafonoff@hellofresh.com.au>
2022-03-07 15:00:22 +1100

    wip

---
+++ fizzbuzz.py
@@ -0,0 +1,4 @@
+def fizzbuzz(number):
+    if number % 3 == 0:
+        return "Fizz"
+    return number
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,4 +1,38 @@
 # using pytest
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number
+# and for the multiples of five print “Buzz”. For numbers which
+# are multiples of both three and five print “FizzBuzz “.
+#
+from fizzbuzz import fizzbuzz
 
-def test_start():
-    assert 1 == 1
+
+def test_fizzbuzz_returns_number():
+    assert fizzbuzz(1) == 1, "Should be 1"
+
+def test_returns_fizz_when_number_divisible_by_three():
+    assert fizzbuzz(3) == "Fizz", "Should be Fizz"
+
+# Sample output:
+#
+# 1
+# 2
+# Fizz
+# 4
+# Buzz
+# Fizz
+# 7
+# 8
+# Fizz
+# Buzz
+# 11
+# Fizz
+# 13
+# 14
+# FizzBuzz
+# 16
+# 17
+# Fizz
+# 19
+# Buzz
+# ... etc up to 100
