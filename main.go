abbf8213c98be4e28bef5df673abe529f7b0c5bd
Ragan Martinez <ragan.martinez@hellofresh.com.au>
2022-03-07 13:41:32 +1100

    refactor

--- main.go
+++ main.go
@@ -8,14 +8,19 @@ func main() {
 
 func FizzBuzz(n int) {
 	for i := 1; i <= n; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz")
-		} else if i%3 == 0 {
-			fmt.Println("Fizz")
-		} else if i%5 == 0 {
-			fmt.Println("Buzz")
-		} else {
-			fmt.Println(i)
-		}
+		fmt.Println(toFizzOrToBuzz(i))
 	}
+}
+
+func toFizzOrToBuzz(n int) string {
+	// or perhaps to FizzBuzz
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
+		return "Fizz"
+	} else if n%5 == 0 {
+		return "Buzz"
+	}
+
+	return fmt.Sprintf("%d", n)
 }
