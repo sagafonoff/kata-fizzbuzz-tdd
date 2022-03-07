2d05b33bd59150cf1a7b228b90e72e0ae1c5024e
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2022-03-07 16:33:25 +1100

    feat: add missing test

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -1,14 +1,21 @@
 import { shoutNumber } from "./fizzbuzz.js";
 
 describe("has a valid result", () => {
+  it("should return a number", () => {
+    const result = shoutNumber(1);
+    expect(result).toBe(1);
+  });
+
   it("should return Fizz", () => {
     const result = shoutNumber(3);
     expect(result).toBe("Fizz");
   });
+
   it("should return Buzz", () => {
     const result = shoutNumber(5);
     expect(result).toBe("Buzz");
   });
+
   it("should return FizzBuzz", () => {
     const result = shoutNumber(15);
     expect(result).toBe("FizzBuzz");
