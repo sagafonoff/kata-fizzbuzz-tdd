edb5fb79b77910e4797af451c76c50f81f24d85f
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2022-03-07 13:33:19 +1100

    feat: add fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+function shoutNumber(numberShouted) {
+  if (numberShouted % 15 === 0) {
+    return "Fizz Buzz";
+  } else if (numberShouted % 3 === 0) {
+    return "Fizz";
+  } else if (numberShouted % 5 === 0) {
+    return "Buzz";
+  }
+  return numberShouted;
+}
+
+for (let counter = 0; counter < 100; counter++) {
+  shoutNumber(counter);
+  console.log(shoutNumber(counter));
+}
